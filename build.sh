21569193b008490905a9810695872ae58690fe18
szykol <szymonkolton@gmail.com>
2019-11-20 19:52:49 +0100

    Add a way to build all projects at once

---
+++ build.sh
@@ -0,0 +1,14 @@
+if hash cmake 2>/dev/null; then
+    mkdir -p build
+    cd build
+    if [ "$1" = "debug" ]; then
+        echo "Running cmake with debug flag"
+        cmake -DCMAKE_BUILD_TYPE=Debug ..
+    else
+        echo "Running cmake without debug flag"
+        cmake ..
+    fi
+    make
+else
+    echo "cmake is not installed. Please try again with cmake installed"
+fi
